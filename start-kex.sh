31d327dc56bd39460aca724b6c4dd102f120ac05
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-09-16 21:23:49 +0630

    Delete start-kex.sh

--- start-kex.sh
+++
@@ -1,3 +0,0 @@
-z="
-";Cz='serv';Jz='xsta';Kz='rtup';Dz='er -';Bz='rvnc';Lz=' /us';Ez='geom';Tz='tcp ';Gz=' 192';Fz='etry';Sz='ten ';Iz='80 -';Az='tige';Pz='sess';Rz='-lis';Qz='ion ';Mz='r/bi';Uz=':1';Nz='n/xf';Oz='ce4-';Hz='0x10';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz"
