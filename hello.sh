9aadabcdb7b9df645f2d6c451ccbb89b290b8b56
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-09-16 21:30:36 +0630

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,3 +1,5 @@
+#by Ad A M [ Mr. Rabbit ]
+#Do not touch my code
 z="
 ";Cz='d';Mz='er "';Oz='art-';Fz='"\e[';Iz='art ';Pz='kex.';Gz='1;32';Bz='assw';Qz='sh';Hz='m St';Nz='./st';Ez=' -e ';Lz='Serv';Kz='VNC ';Jz='the ';Az='vncp';Dz='echo';
 eval "$Az$Bz$Cz$z$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$z$Nz$Oz$Pz$Qz"
