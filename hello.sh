89e7b6c66535a5e15dbb3f7b723ab1df1b4f2c78
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-09-16 21:28:59 +0630

    Update and rename hello.sh to stop-kex.sh

--- hello.sh
+++
@@ -1,3 +0,0 @@
-z="
-";Cz='r --';Dz='kill';Az='vncs';Bz='erve';
-eval "$Az$Bz$Cz$Dz"
---
+++ stop-kex.sh
@@ -0,0 +1,6 @@
+
+#By Ad A M [ Mr .Rabbit ]
+#Do not touch my code
+z="
+";Cz='r --';Dz='kill';Az='vncs';Bz='erve';
+eval "$Az$Bz$Cz$Dz"
