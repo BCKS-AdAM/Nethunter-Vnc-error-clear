8d294ac1bfd4a4cf6424234db3fa823388660f07
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-09-16 21:25:10 +0630

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,3 @@
+z="
+";Sz='ten ';Lz=' /us';Dz='er -';Nz='n/xf';Hz='0x10';Gz=' 192';Fz='etry';Kz='rtup';Cz='serv';Rz='-lis';Iz='80 -';Qz='ion ';Bz='rvnc';Ez='geom';Mz='r/bi';Az='tige';Oz='ce4-';Pz='sess';Jz='xsta';Uz=':1';Tz='tcp ';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz"
